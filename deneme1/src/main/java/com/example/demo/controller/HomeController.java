6629a2707b03ed29c7b7854a046947769dbfbffc
yakup <ylcinyakup@gmail.com>
2019-05-11 13:32:25 +0300

    deneme1
    deneme1

---
+++ deneme1/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.jboss.logging.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value="")
+public class HomeController {
+
+    @GetMapping(value = "e")
+    public ResponseEntity<?> me() {
+        return ResponseEntity.ok("deneme1");
+    }
+}
